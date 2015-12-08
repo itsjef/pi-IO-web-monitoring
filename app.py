5a048f9b678f70cd3c9ab1c24db0a67e6894e2a4
Duc Anh Tran <td.anh0812@gmail.com>
2015-12-08 18:30:08 +0000

    Hello World

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', debug=True)
